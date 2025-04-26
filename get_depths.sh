48cdf9517076b91ba3b16e7be6efc8a6cd587653
akw0088 <123705603+awright2009@users.noreply.github.com>
2025-04-26 13:42:31 -0500

    add changes (print due to anaconda shell not being bash)

--- get_depths.sh
+++ get_depths.sh
@@ -1,6 +1,7 @@
 for file in ./images/*depth.png; do 
     if [ -f "$file" ]; then 
-        python depth_min_max.py "$file"
+	echo "python depth_min_max.py $file"
+        #python depth_min_max.py "$file"
     fi 
 done
 
