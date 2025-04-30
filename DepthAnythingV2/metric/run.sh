5085f4433adcb3408f97fed8bcc42eb3cefe04f5
awright2009 <alexwright@smu.edu>
2025-04-30 16:04:53 -0400

    add metric again

---
+++ DepthAnythingV2/metric/run.sh
@@ -0,0 +1,1 @@
+python run.py   --encoder vitl   --load-from checkpoints/depth_anything_v2_metric_hypersim_vitl.pth   --max-depth 20   --img-path ~/yolo/images/left.JPG --outdir ./output/  --pred-only --grayscale
