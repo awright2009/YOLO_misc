e3e273d21917b5eb06863afa6bd8ceda803e1699
awright2009 <alexwright@smu.edu>
2025-05-05 17:39:08 -0400

    add webcam script

---
+++ webcam2.py
@@ -0,0 +1,46 @@
+import cv2
+from ultralytics import YOLO
+model = YOLO("yolo11x.pt")
+from pathlib import Path
+import os
+import random
+import cv2
+import numpy as np
+
+def predict(chosen_model, img, classes=[], conf=0.5):
+    if classes:
+        results = chosen_model.predict(img, classes=classes, conf=conf)
+    else:
+        results = chosen_model.predict(img, conf=conf)
+
+    return results
+
+def predict_and_detect(chosen_model, img, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
+    results = predict(chosen_model, img, classes, conf=conf)
+    for result in results:
+        for box in result.boxes:
+            cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                          (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), rectangle_thickness)
+            cv2.putText(img, f"{result.names[int(box.cls[0])]}",
+                        (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
+    return img, results
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened():
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    result_img, _ = predict_and_detect(model, frame, classes=[], conf=0.5)
+    cv2.imshow("preview", result_img)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+cv2.destroyWindow("preview")
+vc.release()
